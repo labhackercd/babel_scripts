f9610c592b7b74966a47c93ecb50c3dab62155e9
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-06-12 17:25:52 -0300

    Add pipeline to send data to babel

--- discursos_chile/pipelines.py
+++ discursos_chile/pipelines.py
@@ -4,8 +4,59 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from .items import SenatorItem, InterventionItem
+from decouple import config
+import requests
+BABEL_API_URL = config('BABEL_API_URL',
+                       default='http://localhost:8000/api/v1/')
+CHANNEL_ID = config('CHANNEL_ID',
+                    default=5)
+MANIFESTATION_TYPE_ID = config('MANIFESTATION_TYPE_ID', default=3)
+COLLECT_ID = config('COLLECT_ID', default=3)
 
 
 class DiscursosChilePipeline(object):
+    profiles = {}
+
     def process_item(self, item, spider):
+        if isinstance(item, SenatorItem):
+            author_data = {
+                'name': item['name'],
+                'author_type': 'Senador CL',
+            }
+            author = requests.post(BABEL_API_URL + 'authors/',
+                                   json=author_data).json()
+
+            profile_data = {
+                'channel_id': CHANNEL_ID,
+                'author_id': author['id'],
+                'url': item['url'],
+                'id_in_channel': item['senator_id'],
+                'is_reference': 'true',
+                'attrs': [
+                    {'field': 'name', 'value': item['name']},
+                    {'field': 'party', 'value': item['party']},
+                    {'field': 'phone', 'value': item['phone']},
+                    {'field': 'email', 'value': item['email']},
+                    {'field': 'region', 'value': item['region']},
+                ]
+            }
+            profile = requests.post(BABEL_API_URL + 'profiles/',
+                                    json=profile_data).json()
+            self.profiles[item['senator_id']] = profile['id']
+
+        if isinstance(item, InterventionItem):
+            intervention_data = {
+                'manifestation_type_id': MANIFESTATION_TYPE_ID,
+                'profile_id': self.profiles[item['senator_id']],
+                'collect_id': COLLECT_ID,
+                'id_in_channel': item['url'],
+                'content': item['speech'],
+                'timestamp': item['date'],
+                'url': item['url'],
+                'attrs': [],
+            }
+            requests.post(BABEL_API_URL + 'manifestations/',
+                          json=intervention_data)
+
         return item
