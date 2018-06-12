7cbe6170beb3ce2ff541ba78288e7884a5e898af
Andres Letelier <letelier@gmail.com>
2018-06-12 13:44:03 -0300

    more spiders

--- discursos_chile/spiders/senator_spider.py
+++ discursos_chile/spiders/senator_spider.py
@@ -1,6 +1,8 @@
 import scrapy
 from scrapy.selector import Selector
 from discursos_chile.items import SenatorItem
+from datetime import date
+import intervention_spider
 
 
 class SenatorSpider(scrapy.Spider):
@@ -18,9 +20,27 @@ class SenatorSpider(scrapy.Spider):
         item = SenatorItem()
         item['senator_id'] = response.xpath('//*[@id="parlid"]/@value').extract_first()
         item['name'] = response.xpath('//div[@class="datos"]/../h1/text()').extract_first()
-        item['party'] = response.xpath('//strong[contains(text(),"Partido")]/following-sibling::text()').extract_first().strip()
-        item['phone'] = response.xpath('//strong[contains(text(),"Teléfono")]/following-sibling::text()').extract_first().strip()
-        item['email'] = response.xpath('//strong[contains(text(),"Mail")]/following-sibling::text()').extract_first().strip()
-        item['region'] = response.xpath('//*[@id="main"]/section[1]/div[1]/div[2]/h2[2]/text()').extract_first()
+        item['party'] = response.xpath(
+            '//strong[contains(text(),"Partido")]/following-sibling::text()').extract_first().strip()
+        item['phone'] = response.xpath(
+            '//strong[contains(text(),"Teléfono")]/following-sibling::text()').extract_first().strip()
+        item['email'] = response.xpath(
+            '//strong[contains(text(),"Mail")]/following-sibling::text()').extract_first().strip()
+        item['region'] = response.xpath(
+            '//*[@id="main"]/section[1]/div[1]/div[2]/h2[2]/text()').extract_first()
 
         yield item
+
+        for year in range(2004, date.today().year + 1):
+            yield scrapy.FormRequest(
+                'http://www.senado.cl/appsenado/index.php',
+                intervention_spider.InterventionSpider().parse_intervention_list,
+
+                formdata=dict(
+                    mo='senadores',
+                    ac='intervenciones_senador',
+                    parlamentario=item['senator_id'],
+                    ano=str(year),
+                ),
+
+            )
